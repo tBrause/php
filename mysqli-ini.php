2fed5a32c1a599f47f69195c294e3bb1c1516f8c
Torsten Brause <89082262+tBrause@users.noreply.github.com>
2022-04-22 23:35:40 +0200

    Update mysqli-ini.php

--- mysqli-ini.php
+++ mysqli-ini.php
@@ -1,1 +1,9 @@
+################# connection to the database ###
+$mysql_host             = "localhost";
+$mysql_user             = "root";
+$mysql_password         = "secret";
+$mysql_dbname           = "filmwebsite";
+$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_dbname);
 
+# mysqli charset
+mysqli_set_charset($conn, 'utf8');
