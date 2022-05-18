b264098abf93e15096422f0923397fc15acc19c7
Torsten Brause <89082262+tBrause@users.noreply.github.com>
2022-05-18 18:36:45 +0200

    Update mysqli-ini.php

--- mysqli-ini.php
+++ mysqli-ini.php
@@ -9,3 +9,4 @@ $conn = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_dbname)
 mysqli_set_charset($conn, 'utf8');
 
 $result = mysqli_query($conn, $sql);
+$count = mysqli_num_rows($result);
